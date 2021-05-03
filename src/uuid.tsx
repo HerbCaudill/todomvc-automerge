fc62af59316d7ae9118e2861c1bbb580e22322b7
HerbCaudill <herb@devresults.com>
2021-05-03 14:17:26 +0200

    uuid.tsx:

--- src/uuid.tsx
+++ src/uuid.tsx
@@ -1,13 +1,12 @@
-﻿// https://www.w3resource.com/javascript-exercises/javascript-math-exercise-23.php
-export function uuid() {
-  var dt = new Date().getTime()
-  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
-    /[xy]/g,
-    function (c) {
-      var r = (dt + Math.random() * 16) % 16 | 0
-      dt = Math.floor(dt / 16)
-      return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16)
-    }
-  )
-  return uuid
+﻿export function uuid() {
+  let dt = new Date().getTime()
+
+  function replaceDigit(c: string) {
+    const r = (dt + Math.random() * 16) % 16 | 0
+    dt = Math.floor(dt / 16)
+    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16)
+  }
+
+  const template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+  return template.replace(/[xy]/g, replaceDigit)
 }
