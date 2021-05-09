803eb2cdf263e38c5d8261f9d9432cbf0efd0e72
HerbCaudill <herb@devresults.com>
2021-05-09 14:34:47 +0200

    defaultState.tsx: create

---
+++ src/defaultState.tsx
@@ -0,0 +1,22 @@
+﻿import * as A from 'automerge'
+import { State } from './types'
+
+export const defaultState: A.Doc<State> = A.from({
+  todos: [
+    {
+      id: '0',
+      completed: true,
+      value: 'Learn React',
+    },
+    {
+      id: '1',
+      completed: false,
+      value: 'Learn Automerge',
+    },
+    {
+      id: '2',
+      completed: false,
+      value: 'Profit',
+    },
+  ],
+})
